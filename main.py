78625f56e8037cf03baa0c4204c5e6eaedcaaa3f
yordancastrog7-pixel <yordancastrog7@gmail.com>
2026-04-27 20:44:13 -0500

    API de FastAPI funcionando con éxito

---
+++ main.py
@@ -0,0 +1,9 @@
+from fastapi import FastAPI
+
+app = FastAPI(
+
+title="Mi primera API Yordan appyCastro",
+description="Esta es mi primera API creada con FastAPI yo puedo",
+version = "1.0.0",
+
+)
